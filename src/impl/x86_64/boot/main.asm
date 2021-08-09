a514b6311f030de5b59856750a8f7b4ac5675c9a
ebamberg <erik.bamberg@web.de>
2021-08-09 15:53:08 +0000

    setup stack, memory page table and enter 64bit mode

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -1,9 +1,113 @@
 global header_start
+extern long_mode_start
 section .text
 bits 32
 start:
-    ; print "OK"
-    ; write directly  to video memory
-    mov dword [0xb8000], 0x2f4b2f4f
+    
+    ; ---- set up stack pointer -------
+    mov esp, stack_top          ; esp is the cpu stack-pointer, set stack pointer to our empty stack
+    ; -------------------------------------
+    ;  ----- setup virtual memory paging
+    ; -------------------------------------
+    ; virtual memory paging is necessary to enter long-mode (=64bit)
+    ;
+    call setup_page-tables
+    call enable_paging
+
+// start our 64bit code
+	lgdt [gdt64.pointer]
+	jmp gdt64.code_segment:long_mode_start          ; jump into 64bit assembler code; from here on we are running in 64bit mode
+
     
     hlt
+
+setup_page_tables:
+
+    // we pick adress of page_table_level3, the first 12 bits of the adress are flags, so we or then
+    // then we can write this adress at the start of page_table_level4
+    // so page_table_level4[ adress 0] point to start of page_table_level3
+    // then page_table_level3 points to page_table_level2
+
+    mov eax, page_table_level3
+    // first 12 bits are flags rest points to the table (ore entries are align to 4096)
+    or eax, 0b11 // set preset, writable
+    mov [page_table_level4], eax      
+
+    mov eax, page_table_level2
+    or eax, 0b11 // set present, writable
+    mov [page_table_level3], eax     
+
+    // we map first GB of memory to 512 HugePage block of virtual memory
+    // so virtual memory address is = physical memory for all this 512 huge pages
+    // fill table 2 with pointer to 2MB blocks 512 time 2MB we end up with 1 GB of memory
+    // we have to set a point at every entry of the pagel_table_level2 
+    mov ecx, 0
+loop:
+    move eax, 0x200000   ; 2 MB
+    mul ecx             ; 2mb * Pointer = physical memory adress
+    or eax, 0b10000011  ; set present, writable, huge page
+    move [page_table_level2+ecx*8], eax ; set the page address into  the page_table_level2
+    inc ecx
+    cmp ecx,512
+    jne .loop
+
+    ret
+
+enable_paging:
+    move eax,page_table_level4
+    mov cr3, eax                ; cr3 cpu register points to our page_table
+    ; enable physical address extension
+    mov eax, cr4
+    or eax, 1<< 5       ; enable PAE by setting bit 5 of cr4-cpu-register
+    mov cr4,eax
+
+    ; enable long mode (64bit)
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
+
+	; enable paging
+	mov eax, cr0
+	or eax, 1 << 31
+	mov cr0, eax
+
+    ret
+; --------------------------
+;   RESERVED MEMORY
+; --------------------------
+;
+section .bss
+align 4096
+;
+; --------------------------
+;   reserved Page Table
+; --------------------------
+;
+page_table_level4:
+    resp 4096
+page_table_level3:
+    resp 4096
+page_table_level2:
+    resp 4096
+page_table_level1:
+    resp 4096
+       
+
+;
+; --------------------------
+;   reserved Stack memory
+; --------------------------
+;
+stack_bottom:
+    resb 4096*4     ; 4kb * 4 (=32bit)
+stack_top:          ; a stack is bottom up
+
+section .rodata
+gdt64:
+	dq 0 ; zero entry
+.code_segment: equ $ - gdt64
+	dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
+.pointer:
+	dw $ - gdt64 - 1 ; length
+	dq gdt64 ; address
---
+++ src/impl/x86_64/boot/main64.asm
@@ -0,0 +1,19 @@
+global long_mode_start
+
+
+.section text
+bits 64
+
+long_mode_start:
+    ; load null into all data segment registers, so that the cpu will function correctly
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+	; print "OK"
+    ; write directly  to video memory
+    mov dword [0xb8000], 0x2f4b2f4f
+    hlt
