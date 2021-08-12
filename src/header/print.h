f43f29d523949c6df9e01f5e15b2b1109918328b
ebamberg <erik.bamberg@web.de>
2021-08-12 10:47:31 +0000

    compile bugfixes for c code

--- src/header/print.h
+++ src/header/print.h
@@ -24,6 +24,6 @@ enum {
 
 void print_clear();
 void print_char(char character);
-void print_string(char* string);
-void print_set_colors(uint8_t foreground,uint8_t background);
+void print_str(char* string);
+void print_set_color(uint8_t foreground,uint8_t background);
 
